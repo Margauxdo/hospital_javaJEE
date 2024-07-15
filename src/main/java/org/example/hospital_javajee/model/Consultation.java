356022a01dbf1758c62e5e362d5b4972ea92cb8e
Margaux Doisy <margauxdoisy@gmail.com>
2024-07-15 21:53:37 +0200

    creation entité

--- src/main/java/org/example/hospital_javajee/model/Consultation.java
+++ src/main/java/org/example/hospital_javajee/model/Consultation.java
@@ -1,11 +1,28 @@
 package org.example.hospital_javajee.model;
 
-import java.time.LocalDate;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jdk.jfr.Name;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 
+import java.time.LocalDate;
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
 public class Consultation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String doctor;
     private LocalDate date;
     private String prescription;
     private String ficheSoin;
+
+
 }
--- src/main/java/org/example/hospital_javajee/model/Patient.java
+++ src/main/java/org/example/hospital_javajee/model/Patient.java
@@ -1,17 +1,32 @@
 package org.example.hospital_javajee.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDate;
 import java.util.List;
 
 
 //entité et relation
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String firstname;
+    @Temporal(TemporalType.DATE)
     private LocalDate dateOfBirth;
+    @OneToMany(mappedBy = "patient")
     private List<Consultation> consultations;
-    //type img
+
+
 }
